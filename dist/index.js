1bb1da2979d3a1c7b4d50053e5c4a0dec0a2b73d
mochizuki.kohei <mochizuki.kohei@jp.panasonic.com>
2025-02-19 17:45:40 +0900

    Delete unnecessary characters before and after author

--- dist/index.js
+++ dist/index.js
@@ -31524,9 +31524,8 @@ const getChangedFiles = async (sha, execOptions) => {
  */
 const getCommitAuthor = async (execOptions) => {
   const { stdout: author } = await execExports.getExecOutput('git', ['log', '-1', '--pretty=format:"%an"'], execOptions);
-  return author
+  return author.replace(/"/g, '') // Remove quotes from the author name
 };
-
 /**
  * Generates the body object for the custom action.
  *
