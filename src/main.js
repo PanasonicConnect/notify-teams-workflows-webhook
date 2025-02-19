1eb08f9467478787df61ee26f0aec942d1fd4b14
mochizuki.kohei <mochizuki.kohei@jp.panasonic.com>
2025-02-19 18:06:39 +0900

    Corrected trimming process

--- src/main.js
+++ src/main.js
@@ -71,7 +71,7 @@ const getChangedFiles = async (sha, execOptions) => {
  */
 const getCommitAuthor = async (execOptions) => {
   const { stdout: author } = await exec.getExecOutput('git', ['log', '-1', '--pretty=format:%an'], execOptions)
-  return author.replace(/"/g, '') // Remove quotes from the author name
+  return author.replace(/\\/g, '') // Remove quotes from the author name
 }
 /**
  * Generates the body object for the custom action.
